0f83e2754944f6457009fb2e7ccf6d98e9ee476f
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-04-19 10:42:32 +0530

    Feat: Added error handler middleware with basic error classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,13 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
+const NotImplemented = require("../errors/notImplemented.error");
 
 function pingProblemController(req, res) {
   return res.json({ message: "Ping controller is active" });
 }
 
-function addProblem(req, res) {
-  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-    message: "Not Implemented",
-  });
+function addProblem(req, res, next) {
+  try {
+    throw new NotImplemented("addProblem");
+  } catch (error) {
+    next(error);
+  }
 }
 
 function getProblem(req, res) {
